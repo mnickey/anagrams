faaa48aa6602e47d8f0f10066eb6b8d61691b724
Michael Nickey <mnickey@gmail.com>
2015-04-20 22:07:52 -0700

    Templates finished, working on logic

--- anagrams.py
+++ anagrams.py
@@ -4,15 +4,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'get':
+    if request.method == 'GET':
         return render_template('main.html')
     else:
-        myLetters = request.form(['letters'])
+        myLetters = request.form['letters']
+        myLetters = ''.join(sorted(myLetters))
         myLetters = myLetters.strip()
         myLetters = sorted(myLetters)
-        myLetters = myLetters.upper()
-        # myLetters = list(myLetters)
-        # myLetters = ''.join(sorted(myLetters))
+
         return render_template('solver.html', myLetters = myLetters)
 
 if __name__ == '__main__':
