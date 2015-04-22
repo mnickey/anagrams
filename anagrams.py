a41764273bc5ffea1dc8574ed218c817b162f407
Michael Nickey <mnickey@gmail.com>
2015-04-21 23:28:39 -0700

    It's alive. ALIVE! (logic issues resolved)

--- anagrams.py
+++ anagrams.py
@@ -1,50 +1,29 @@
 from flask import Flask, render_template, request
-from itertools import permutations
 
 app = Flask(__name__)
+
+""" This is setting up the control dictionary to read against """
+from collections import defaultdict
+words = defaultdict(list)
+with open("/usr/share/dict/words") as f:
+    for word in f:
+        word=word.strip()
+        words[''.join(sorted(word))].append(word)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('main.html')
     else:
+        #this is the original set of letters that I want anagrams for
         myLetters = request.form['letters']
+        # some cleanup on those letters
         myLetters = ''.join(sorted(myLetters))
-        myLetters = myLetters.strip()
+        # then assign those letters to 'word'
+        word = myLetters.strip().lower()
 
-        myWords = []
-        myLetterList = list(myLetters)
-        # lettersLength = len(myLetterList)
-        # myWords = [''.join(result) for result in permutations(myLetters)]
-        # myWords = sorted(myWords)
-
-        # with open("/usr/share/dict/words") as defaultWords:
-        #     words = set(line.strip() for line in defaultWords)
-        #     for word in words:
-        #         if word not in words:
-        #             myWords.remove(word)
-        #             defaultWords.seek(0) #move the seek position back to original area.
-
-
-        #From rousav at Thinkful
-        from collections import defaultdict
-        words = defaultdict(list)
-        with open("/usr/share/dict/words") as f:
-            for word in f:
-                word=word.strip()
-                words[''.join(sorted(word))].append(word)
-
-        import sys
-        for word in words:
-            myWords = (words[''.join(sorted(word))])
-
-            # for x in myLetterList:
-            #     for word in defaultWords:
-            #         if x in word and len(word) == (lettersLength + 1):
-            #             if word in myWords: #Check for existing word
-            #                 continue
-            #             else:
-            #                 myWords.append(word)
+        """ This is where I need to check the letter sets against the control group """
+        myWords =  words[''.join(sorted(word))]
         return render_template('solver.html', myLetters = myLetters, myWords = myWords)
 
 if __name__ == '__main__':
--- solver_file.py
+++ solver_file.py
@@ -1,14 +1,29 @@
-__author__ = 'MNickey'
-
-from collections import defaultdict
-words = defaultdict(list)
-with open("/usr/share/dict/words") as f:
-    for word in f:
-        word=word.strip()
-        words[''.join(sorted(word))].append(word)
+__author__ = 'Chris Angellico'
+# Modified by: 'Michael Nickey'
 
 import sys
-for word in sys.argv[1:]:
-    myWords = (words[''.join(sorted(word))])
+from collections import defaultdict
+words = defaultdict(list)
+myWords = []
 
-print "This is myWords", myWords
+def find_anagrams(letters):
+    with open("/usr/share/dict/words") as f:
+        for word in f:
+            word=word.strip()
+            words[''.join(sorted(word))].append(word)
+    for word in sys.argv[1:]:
+        myWords.append( [''.join(sorted(word))] )
+    return myWords
+
+
+# Original Work
+# from collections import defaultdict
+# words = defaultdict(list)
+# with open("/usr/share/dict/words") as f:
+#     for word in f:
+#         word=word.strip()
+#         words[''.join(sorted(word))].append(word)
+#
+# import sys
+# for word in sys.argv[1:]:
+#     print(words[''.join(sorted(word))])
